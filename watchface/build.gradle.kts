10c3792c0f3ae926651380f48b3a0c5c949a8848
tomatojuice <tomato.juice.7@gmail.com>
2026-04-20 12:10:10 +0900

    update

--- watchface/build.gradle.kts
+++ watchface/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.ne.sakura.tomatojuice.apexface"
         minSdk = 34
         targetSdk = 34
-        versionCode = 11000005
-        versionName = "3.1.3"
+        versionCode = 11000006
+        versionName = "3.1.4"
     }
 
     // 🌟 徹底掃除 1: 警告が出ていた部分を最新の書き方(directories.clear)に修正
